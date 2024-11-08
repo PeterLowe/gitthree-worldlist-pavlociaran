7e278a46d02b654a3994da8e94ee879c8797e734
(Student  C00285455) Ciaran Kinsella <C00285455@setu.ie>
2024-11-08 11:12:18 +0000

    added my name to main

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -1,8 +1,7 @@
 // program to find countries in Pete's World
-// date November 2022
+// date 08 November 2024
 // author Pete Lowe
-// add your name here
-//  or here
+// Author Ciaran Kinsella
 // estimated time 120 minutes
 // actual time ??
 // version 1.0
